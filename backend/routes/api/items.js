a343595731cfebf893733b094d627d278c7aa802
Etay Goz <89639118+Etgoz@users.noreply.github.com>
2023-04-02 09:30:39 +0000

    placeholder image api

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -132,10 +132,6 @@ router.get('/feed', auth.required, function (req, res, next) {
 
 				return res.json({
 					items: items.map(function (item) {
-						item.image = item.image
-							? item.image
-							: 'https://github.com/ObelusFamily/Anythink-Market-j7pq7l0t/blob/main/frontend/src/imgs/placeholder.png?raw=true';
-
 						return item.toJSONFor(user);
 					}),
 					itemsCount: itemsCount,
